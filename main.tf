de2a8d917c773f8a698b405a0fc455b424c7c365
Wellington750 <wellingtosw@gmail.com>
2022-04-22 14:17:17 -0300

    Alter: main

--- main.tf
+++ main.tf
@@ -6,22 +6,21 @@ module "vpc" {
 
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = false
-  enable_vpn_gateway = false
+  one_nat_gateway_per_az = false
 
   tags = {
-    Terraform   = "true"
+    Terraform = "true"
     Environment = "Production"
-    CC          = "10504"
-    OwnerSquad  = "SRE"
-    OwnerSRE    = "Teste"
+    CC = "10504"
+    OwnerSquad = "Osaka"
+    OwnerSRE = "Valfenda"
   }
 }
 
 output "vpc_vorx_prod_id" {
-  value = module.vpc.vpc_id
-
+    value = module.vpc.vpc_id
 }
